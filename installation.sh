53759db48fb8c4cc15006a22f36ec33cf3d95de6
BeLeFu <59803670+BeLeFu@users.noreply.github.com>
2020-03-13 08:41:33 +0100

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update # Gleicht den Paketindex der bekannten Repositories mit ihren Servern ab
-sudo apt install unclutter xorg matchbox-window-manager # Installiert folgendes:
+sudo apt install unclutter xorg matchbox-window-manager -y # Installiert folgendes:
 
 # Unclutter - Programm zum Verstecken des Mauszeigers 
 # Xorg - Grafische Oberfläche
@@ -9,13 +9,13 @@ sudo apt install unclutter xorg matchbox-window-manager # Installiert folgendes:
 
 cd /VPLAN # Wechselt in das Verzeichnis /VPLAN
 
-rm /etc/rc.local # Löscht die Datei rc.local
-mv /VPLAN/rc.local /etc/rc.local # Fügt unsere eigene rc.local - Datei ein
+sudo rm /etc/rc.local # Löscht die Datei rc.local
+sudo mv /VPLAN/rc.local /etc/rc.local # Fügt unsere eigene rc.local - Datei ein
 
 # Zur Funktion der Datei rc.local:
 # Das Script rc.local wird während des Bootvorgangs ausgeführt und ist genau für unseren Gebrauch geeignet - Der AUsführung von Programmenn und Scripten direkt nach dem Booten - Vergleichbar mit der Autostart - Funktion von Windows.
 
-mv /VPLAN/override.conf /etc/systemd/system/getty@tty1.service.d/ # Fügt unsere override.conf - Datei ein
+sudo mv /VPLAN/override.conf /etc/systemd/system/getty@tty1.service.d/ # Fügt unsere override.conf - Datei ein
 
 # Zur Funktion von override.conf:
 # Durch diese Konfigurationsdatei loggt sich unser System ohne unser Zutun ein.
